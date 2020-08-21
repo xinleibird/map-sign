24eb704f40dc789400e21b4701335081b8196189
xinleibird <xinleibird@gmail.com>
2020-08-21 23:22:16 +0800

    Updates

---
+++ precache-manifest.bee9ed0fdbb65108c6e7efa9c8b19701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7ac0b869da19cc6d5618e8fe1867a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd40ccef3f5606e908b3",
+    "url": "/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "d092a0bb199ac71a64e5",
+    "url": "/static/css/main.cc84a05d.chunk.css"
+  },
+  {
+    "revision": "dd40ccef3f5606e908b3",
+    "url": "/static/js/2.6fa33b98.chunk.js"
+  },
+  {
+    "revision": "bfe0dd24683b14f95837b81890aec504",
+    "url": "/static/js/2.6fa33b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d092a0bb199ac71a64e5",
+    "url": "/static/js/main.b6a34423.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
