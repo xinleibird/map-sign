f88971ac62c2dde2caa3671247834d5902c6703c
xinleibird <xinleibird@gmail.com>
2020-08-24 08:31:05 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/map-sign/precache-manifest.bf4b8244cd6de2f4ac1d444d3e51956f.js"
+  "/map-sign/precache-manifest.937038835c76182e7918f884b58133e9.js"
 );
 
 self.addEventListener('message', (event) => {
