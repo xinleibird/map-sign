87ec070883c8b8a6d2d8df0708639a8048335f6b
xinleibird <xinleibird@gmail.com>
2020-08-28 03:13:21 +0800

    Updates

---
+++ precache-manifest.2ed20aa7ccb6547224e8f2f733e9bf44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3377ed7f9e232f91fe9d91022cec4b9",
+    "url": "/map-sign/index.html"
+  },
+  {
+    "revision": "0a263381787824af3f47",
+    "url": "/map-sign/static/css/2.632c9957.chunk.css"
+  },
+  {
+    "revision": "d529ee41a9420f7ddfc6",
+    "url": "/map-sign/static/css/main.df6d4e3e.chunk.css"
+  },
+  {
+    "revision": "0a263381787824af3f47",
+    "url": "/map-sign/static/js/2.3d550b34.chunk.js"
+  },
+  {
+    "revision": "bfe0dd24683b14f95837b81890aec504",
+    "url": "/map-sign/static/js/2.3d550b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d529ee41a9420f7ddfc6",
+    "url": "/map-sign/static/js/main.5524e689.chunk.js"
+  },
+  {
+    "revision": "82c0a53523ae0f7d6cc7",
+    "url": "/map-sign/static/js/runtime-main.2409df95.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/map-sign/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/map-sign/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "f12b54df29a2d1afa06a875da1d22ef5",
+    "url": "/map-sign/static/media/user.f12b54df.svg"
+  }
+]);
