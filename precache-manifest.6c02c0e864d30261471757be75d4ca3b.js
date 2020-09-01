9613d4d8cbff95393489daec0a0b5dc320acc0b6
xinleibird <xinleibird@gmail.com>
2020-09-01 13:47:48 +0800

    Updates

---
+++ precache-manifest.6c02c0e864d30261471757be75d4ca3b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e4ce31db90ff0e7a397321a4519bec",
+    "url": "/map-sign/index.html"
+  },
+  {
+    "revision": "e8e01a0dc6adc7ca7beb",
+    "url": "/map-sign/static/css/2.632c9957.chunk.css"
+  },
+  {
+    "revision": "b4ecc25b729d6bcc3102",
+    "url": "/map-sign/static/css/main.df6d4e3e.chunk.css"
+  },
+  {
+    "revision": "e8e01a0dc6adc7ca7beb",
+    "url": "/map-sign/static/js/2.fbd85ed1.chunk.js"
+  },
+  {
+    "revision": "bfe0dd24683b14f95837b81890aec504",
+    "url": "/map-sign/static/js/2.fbd85ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ecc25b729d6bcc3102",
+    "url": "/map-sign/static/js/main.1eecce4a.chunk.js"
+  },
+  {
+    "revision": "82c0a53523ae0f7d6cc7",
+    "url": "/map-sign/static/js/runtime-main.2409df95.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/map-sign/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/map-sign/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "f12b54df29a2d1afa06a875da1d22ef5",
+    "url": "/map-sign/static/media/user.f12b54df.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/map-sign/precache-manifest.05b9c2a4d072d9161640e76c0196d64e.js"
+  "/map-sign/precache-manifest.6c02c0e864d30261471757be75d4ca3b.js"
 );
 
 self.addEventListener('message', (event) => {
