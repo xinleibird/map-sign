7e7a27d255e2113a34f9391392d23c4e0a1a54e3
xinleibird <xinleibird@gmail.com>
2020-09-03 15:33:45 +0800

    update server tsconfig

--- server/src/middleware/handleAccess.ts
+++ server/src/middleware/handleAccess.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 const handleAccess = (req: Request, res: Response, next: NextFunction) => {
   if (req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE') {
-    if (req.session.map_sign_user_info) {
+    if (req.session?.map_sign_user_info) {
       next();
     } else {
       next(new Error('Denied!'));
